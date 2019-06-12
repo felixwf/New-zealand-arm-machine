55a3e220e2ce359ce3fe6707e1dd4bf36b9069c8
SZZY\Felix.Wu <felix@zuru.tech>
2019-06-12 18:55:08 +0800

    clean the project

--- MDK-ARM/New zealand arm mathine/startup_stm32f103xb.d
+++
@@ -1,1 +0,0 @@
-new zealand arm mathine\startup_stm32f103xb.o: startup_stm32f103xb.s
--- MDK-ARM/New zealand arm mathine/stepper_msg.d
+++
@@ -1,3 +0,0 @@
-new zealand arm mathine\stepper_msg.o: APP\Stepper\stepper_msg.c
-new zealand arm mathine\stepper_msg.o: APP\Stepper\stepper_msg.h
-new zealand arm mathine\stepper_msg.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
